bd95ff56b2d6c791312db507df0458d29def238f
oguzm <oguzm@OGUZ-PC>
2022-06-02 13:14:00 +0300

    Add project files.

---
+++ Controllers/ShipperController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NorthwindDemoMVC.Models;
+
+namespace NorthwindDemoMVC.Controllers
+{
+    public class ShipperController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: Shipper
+        public ActionResult Index()
+        {
+            var shippers = db.Shippers.Include(s => s.Region);
+            return View(shippers.ToList());
+        }
+        public ActionResult Index2()
+        {
+            var shippers = db.Shippers.Include(s => s.Region);
+            return View(shippers.ToList());
+        }
+
+        // GET: Shipper/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shippers shippers = db.Shippers.Find(id);
+            if (shippers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shippers);
+        }
+
+        // GET: Shipper/Create
+        public ActionResult Create()
+        {
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription");
+            return View();
+        }
+
+        // POST: Shipper/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShipperID,RegionID,CompanyName,Phone")] Shippers shippers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shippers.Add(shippers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription", shippers.RegionID);
+            return View(shippers);
+        }
+
+        // GET: Shipper/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shippers shippers = db.Shippers.Find(id);
+            if (shippers == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription", shippers.RegionID);
+            return View(shippers);
+        }
+
+        // POST: Shipper/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShipperID,RegionID,CompanyName,Phone")] Shippers shippers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shippers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription", shippers.RegionID);
+            return View(shippers);
+        }
+
+        // GET: Shipper/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shippers shippers = db.Shippers.Find(id);
+            if (shippers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shippers);
+        }
+
+        // POST: Shipper/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Shippers shippers = db.Shippers.Find(id);
+            db.Shippers.Remove(shippers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
